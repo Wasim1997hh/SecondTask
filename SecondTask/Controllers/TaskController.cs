dab33bfe78c3fb708bc478d199626f93b294ce0a
Wasim Al-Haddad <wasim1997ans@hotmail.com>
2022-09-07 12:36:56 +0300

    Add project files.

---
+++ SecondTask/Controllers/TaskController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecondTask.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SecondTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly SecondTaskContext _secondTaskContext;
+        public TaskController(SecondTaskContext secondTaskContext)
+        {
+            _secondTaskContext = secondTaskContext;
+        }
+
+        // GET: api/<TaskController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var cc = _secondTaskContext.Items.Select(x => new { x.Id, x.Name,x.SubCategoryId,x.SubCategory  }).ToList();
+            return Ok(cc);
+        }
+        //[HttpGet]
+        //public IActionResult Export()
+        //{
+           
+        //    var cc = _secondTaskContext.Items.Select(x => new { x.Id, x.Name, x.SubCategory }).ToList();
+        //    var builder = new StringBuilder();
+        //    builder.AppendLine("Id,Name");
+        //    foreach (var item in cc)
+        //    {
+        //        builder.AppendLine($"{item.Id} , {item.Name}");
+        //    }
+        //    var export = File(Encoding.UTF8.GetBytes(builder.ToString()), "text/csv", "cc.csv");
+        //    return Ok(export);
+        //}
+
+        // GET api/<TaskController>/5
+       
+
+        // POST api/<TaskController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TaskController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TaskController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SecondTask/Models/SecondTaskContext.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace SecondTask.Models
+{
+    public partial class SecondTaskContext : DbContext
+    {
+        public SecondTaskContext()
+        {
+        }
+
+        public SecondTaskContext(DbContextOptions<SecondTaskContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Item> Items { get; set; }
+        public virtual DbSet<SubCategory> SubCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-M2D7N0P;Database=SecondTask;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.ToTable("Category");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(150);
+            });
+
+            modelBuilder.Entity<Item>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.SubCategoryId).HasColumnName("SubCategory_Id");
+
+                entity.HasOne(d => d.SubCategory)
+                    .WithMany(p => p.Items)
+                    .HasForeignKey(d => d.SubCategoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Items_SubCategory");
+            });
+
+            modelBuilder.Entity<SubCategory>(entity =>
+            {
+                entity.ToTable("SubCategory");
+
+                entity.Property(e => e.CategoryId).HasColumnName("Category_Id");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(150);
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.SubCategories)
+                    .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_SubCategory_Category");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
